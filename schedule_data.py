7235dcc3e91f212a52698e2b3d2f4ba9f2b5bf69
Babu159753 <mustafojonxalimov@gmail.com>
2025-04-12 20:58:20 +0300

    Create schedule_data.py

---
+++ schedule_data.py
@@ -0,0 +1,53 @@
+SCHEDULE = {
+    "CS-01-23": {
+        "Monday": [
+            {"time": "13:00 - 13:50", "course": "Data Warehouse & Data Mining, Cryptography & Blockchain Technology", "location": "Room-5,6"},
+            {"time": "14:00 - 14:50", "course": "Data Warehouse & Data Mining, Cryptography & Blockchain Technology", "location": "Room-5,6"},
+            {"time": "15:00 - 15:50", "course": "UI & UX, Computer Forensics", "location": "Room-5,6"},
+            {"time": "16:00 - 16:50", "course": "UI & UX, Computer Forensics", "location": "Room-5,6"}
+        ],
+        "Tuesday": [
+            {"time": "13:00 - 13:50", "course": "Web Programming- I( HTML, CSS Javascript)", "location": "Room-5"},
+            {"time": "14:00 - 14:50", "course": "Web Programming- I( HTML, CSS Javascript)", "location": "Room-5"},
+            {"time": "15:00 - 15:50", "course": "TUTORING", "location": ""},
+            {"time": "16:00 - 16:50", "course": "Management and Entrepreneurship for IT Industry", "location": ""}
+        ],
+        "Wednesday": [
+            {"time": "12:00 - 12:50", "course": "UI & UX, Computer Forensics", "location": "Room-5,6"},
+            {"time": "13:00 - 13:50", "course": "Compiler Design", "location": "Room-5"},
+            {"time": "14:00 - 14:50", "course": "Compiler Design", "location": "Room-5"},
+            {"time": "15:00 - 15:50", "course": "Cyber Security", "location": "Room-5"}
+        ],
+        "Thursday": [
+            {"time": "12:00 - 12:50", "course": "Data Warehouse & Data Mining", "location": "Room-3"},
+            {"time": "13:00 - 13:50", "course": "Cryptography & Blockchain Technology, Data Warehouse & Data Mining", "location": "Room-5"},
+            {"time": "14:00 - 14:50", "course": "Compiler Design", "location": "Room-5"},
+            {"time": "15:00 - 15:50", "course": "Web Programming- I( HTML, CSS Javascript)", "location": "Room-5"},
+            {"time": "16:00 - 16:50", "course": "Web Programming- I( HTML, CSS Javascript)", "location": "Room-5"}
+        ],
+        "Friday": [
+            {"time": "13:00 - 13:50", "course": "UI & UX", "location": "Room-5"},
+            {"time": "14:00 - 14:50", "course": "Cyber Security", "location": "Room-5"},
+            {"time": "15:00 - 15:50", "course": "Cyber Security", "location": "Room-5"}
+        ],
+        "Saturday": [],
+        "Sunday": []
+    },
+    "CS-02-23": {
+        "Monday": [
+            {"time": "12:00 - 12:50", "course": "Network Programming, Principles of Data Science", "location": "Room-5,6"},
+            {"time": "13:00 - 13:50", "course": "Complex Analysis, Probability, and Statistical Methods", "location": "Room-1"},
+            {"time": "14:00 - 14:50", "course": "Complex Analysis, Probability, and Statistical Methods", "location": "Room-1"},
+            {"time": "15:00 - 15:50", "course": "Operating Systems", "location": ""},
+            {"time": "16:00 - 16:50", "course": "Operating Systems", "location": ""}
+        ],
+        "Tuesday": [
+            {"time": "13:00 - 13:50", "course": "Database Management Systems", "location": "Room-6"}
+        ],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [],
+        "Sunday": []
+    }
+}
